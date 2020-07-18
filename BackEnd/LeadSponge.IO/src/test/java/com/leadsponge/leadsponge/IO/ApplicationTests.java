a63b64a9e7cb17bd007c8546a0835f07507ff2d2
Danton <danton.issler18@gmail.com>
2020-07-18 00:08:23 -0400

    Ajeitando o projeto para criar o jar.

--- BackEnd/LeadSponge.IO/src/test/java/com/leadsponge/leadsponge/IO/ApplicationTests.java
+++ BackEnd/LeadSponge.IO/src/test/java/com/leadsponge/leadsponge/IO/ApplicationTests.java
@@ -1,9 +1,16 @@
 package com.leadsponge.leadsponge.IO;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+import com.leadsponge.IO.LeadSpongeApiApplication;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes=LeadSpongeApiApplication.class)
+@SpringBootTest(classes={LeadSpongeApiApplication.class})
 class ApplicationTests {
 
 	@Test
