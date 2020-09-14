afd3cd43de811b8067281832ed362bed9b1e232c
Danton <danton.issler18@gmail.com>
2020-09-14 14:13:31 -0400

    Colocando o Lombok na classe proporties e criando uma version.

--- BackEnd/LeadSponge.IO/src/main/java/com/leadsponge/IO/config/property/LeadSpongeApiProperty.java
+++ BackEnd/LeadSponge.IO/src/main/java/com/leadsponge/IO/config/property/LeadSpongeApiProperty.java
@@ -2,200 +2,59 @@ package com.leadsponge.IO.config.property;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 @ConfigurationProperties("leadsponge")
 public class LeadSpongeApiProperty {
 
 	private final Seguranca seguranca = new Seguranca();
-
 	private final Mail mail = new Mail();
-
 	private final Disco disco = new Disco();
-
 	private final S3 s3 = new S3();
-
 	private final BancoMysql bancoMysql = new BancoMysql();
 
-	public BancoMysql getBancoMysql() {
-		return bancoMysql;
-	}
-
-	public S3 getS3() {
-		return s3;
-	}
-
-	public Mail getMail() {
-		return mail;
-	}
-
-	public Seguranca getSeguranca() {
-		return seguranca;
-	}
-
-	public Disco getDisco() {
-		return disco;
-	}
-
+	@Getter
+	@Setter
 	public class BancoMysql {
-
 		private String username;
 		private String password;
 		private String host;
 		private String port;
 
-		public String getUsername() {
-			return username;
-		}
-
-		public void setUsername(String username) {
-			this.username = username;
-		}
-
-		public String getPassword() {
-			return password;
-		}
-
-		public void setPassword(String password) {
-			this.password = password;
-		}
-
-		public String getHost() {
-			return host;
-		}
-
-		public void setHost(String host) {
-			this.host = host;
-		}
-
-		public String getPort() {
-			return port;
-		}
-
-		public void setPort(String port) {
-			this.port = port;
-		}
-
 	}
 
+	@Getter
+	@Setter
 	public class Disco {
 		private String raiz;
-
 		private String diretorioFotos;
-
-		public String getRaiz() {
-			return raiz;
-		}
-
-		public void setRaiz(String raiz) {
-			this.raiz = raiz;
-		}
-
-		public String getDiretorioFotos() {
-			return diretorioFotos;
-		}
-
-		public void setDiretorioFotos(String diretorioFotos) {
-			this.diretorioFotos = diretorioFotos;
-		}
 	}
 
+	@Getter
+	@Setter
 	public class Mail {
-
 		private String host;
-
 		private Integer port;
-
 		private String username;
-
 		private String password;
-
-		public String getHost() {
-			return host;
-		}
-
-		public void setHost(String host) {
-			this.host = host;
-		}
-
-		public Integer getPort() {
-			return port;
-		}
-
-		public void setPort(Integer port) {
-			this.port = port;
-		}
-
-		public String getUsername() {
-			return username;
-		}
-
-		public void setUsername(String username) {
-			this.username = username;
-		}
-
-		public String getPassword() {
-			return password;
-		}
-
-		public void setPassword(String password) {
-			this.password = password;
-		}
 	}
 
+	@Getter
+	@Setter
 	public class S3 {
-
 		private String accessKeyId;
-
 		private String secretAccessKey;
-
 		private String bucket;
-
-		public String getBucket() {
-			return bucket;
-		}
-
-		public void setBucket(String bucket) {
-			this.bucket = bucket;
-		}
-
-		public String getAccessKeyId() {
-			return accessKeyId;
-		}
-
-		public void setAccessKeyId(String accessKeyId) {
-			this.accessKeyId = accessKeyId;
-		}
-
-		public String getSecretAccessKey() {
-			return secretAccessKey;
-		}
-
-		public void setSecretAccessKey(String secretAccessKey) {
-			this.secretAccessKey = secretAccessKey;
-		}
-
 	}
 
+	@Getter
+	@Setter
 	public class Seguranca {
-
 		private String originPermitida;
-
 		private boolean enableHttps;
-
-		public String getOriginPermitida() {
-			return originPermitida;
-		}
-
-		public void setOriginPermitida(String originPermitida) {
-			this.originPermitida = originPermitida;
-		}
-
-		public boolean isEnableHttps() {
-			return enableHttps;
-		}
-
-		public void setEnableHttps(boolean enableHttps) {
-			this.enableHttps = enableHttps;
-		}
+		private String version;
 	}
-
 }
