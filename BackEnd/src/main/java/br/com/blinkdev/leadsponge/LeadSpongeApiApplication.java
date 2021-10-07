5d1e9ff357fb17e2481f0d2e0576cccfc464098e
Danton Issler Rodrigues <danton.issler18@gmail.com>
2021-10-07 13:50:47 -0400

    Adicionando bean para o funcionamento do grafana.

--- BackEnd/src/main/java/br/com/blinkdev/leadsponge/LeadSpongeApiApplication.java
+++ BackEnd/src/main/java/br/com/blinkdev/leadsponge/LeadSpongeApiApplication.java
@@ -1,10 +1,14 @@
 package br.com.blinkdev.leadsponge;
 
 import br.com.blinkdev.leadsponge.config.property.LeadSpongeApiProperty;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigurationProperties(LeadSpongeApiProperty.class)
@@ -14,4 +18,9 @@ public class LeadSpongeApiApplication extends SpringBootServletInitializer {
         SpringApplication.run(LeadSpongeApiApplication.class, args);
     }
 
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> configurer(
+            @Value("${spring.application.name}") String applicationName) {
+        return (registry) -> registry.config().commonTags("application", applicationName);
+    }
 }
